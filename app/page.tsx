170905c56636e01c6e65d2b92dfc9bdc56a2f0e9
imai <24ek011@ms.dendai.ac.jp>
2024-09-29 10:51:12 +0900

    URLREDIRECT

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 
@@ -7,58 +7,92 @@ export default function Home() {
 
   const [password, setPassword] = useState<string>("")
 
-  // const { unityProvider } = useUnityContext({
-  //   loaderUrl: "Test.loader.js",
-  //   dataUrl: "Test.data",
-  //   frameworkUrl: "Test.framework.js",
-  //   codeUrl: "Test.wasm",
-  // });
+  const { unityProvider,unload } = useUnityContext({
+    loaderUrl: "Test Fifth.loader.js",
+    dataUrl: "Test Fifth.data",
+    frameworkUrl: "Test Fifth.framework.js",
+    codeUrl: "Test Fifth.wasm",
+  });
 
-  // return <Unity unityProvider={unityProvider} />;
-  const gameClear = true;
+
   // return(
   //   <div>
   //     <h1>タイトル</h1>
   //     {
-  //       gameClear ? <div>You passed.</div> : <div>Please try again.</div>
+  //       isSuccess ? <div>You passed.</div> : <div>Please try again.</div>
   //     }
   //     <p>説明～～</p>
   //   </div>
   // );
 
+  const [isSuccess, setIsSuccess] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // @ts-ignore
+      if (window.isSuccess) {
+        // @ts-ignore
+        setIsSuccess(true);
+        unload()
+      }
+    }, 300); // 1秒ごとに監視
+
+    return () => clearInterval(interval); // コンポーネントがアンマウントされたときにクリーンアップ
+  }, [isSuccess]);
+
   // const password=123;
 
   const checkPass = () => {
     if (password == "123") {
-      alert('認証成功');
-
+      console.log('hogehoge')
+      alert(`認証成功}`)
+      window.location.href = "https://www.dendai.ac.jp/";
     } else {
       alert('パスワードが間違っています');
     }
 
   }
 
-  if (gameClear) {
-    return (
-      <div>
+  return (
+    //     <div>
+    //       <h1>You passed.</h1>
+
+    //       <input type="password" id="password" name="password" placeholder="パスワードを入力"
+    //       value={password}
+    //         onChange={(e) => {
+    //           setPassword(e.target.value)
+    //         }}
+
+
+    //         style={{ backgroundColor: "#f0f0f0", color: "#333", border: "1px solid #ccc", padding: "10px", borderRadius: "5px" }} />
+    //       <input type="submit" name="submit" onClick={checkPass} />
+    //     </div>
+    //   )
+
+
+    // } else {
+    //   return (
+    //     <div>
+    //       <h1>Please try again.</h1>
+    //       <Unity unityProvider={unityProvider} />;
+
+    //     </div>
+    <>
+      {isSuccess ? (<>
         <h1>You passed.</h1>
 
         <input type="password" id="password" name="password" placeholder="パスワードを入力"
+          value={password}
           onChange={(e) => {
             setPassword(e.target.value)
-
           }}
           style={{ backgroundColor: "#f0f0f0", color: "#333", border: "1px solid #ccc", padding: "10px", borderRadius: "5px" }} />
         <input type="submit" name="submit" onClick={checkPass} />
-      </div>
-    )
 
-
-  } else {
-    return (
-      <div>
-        <h1>Please try again.</h1>
-      </div>
-    );
-  }
+      </>) : (<>
+      <Unity unityProvider={unityProvider} />
+      </>)}
+    </>
+  )
 }
+
